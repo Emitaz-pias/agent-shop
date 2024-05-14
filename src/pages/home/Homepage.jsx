60dbba41e3817ab24ce066b8d4b4a7a5a8aaf802
Emtiaz-pias <emtiazpias383@gmail.com>
2024-05-14 14:32:34 +0600

    picture added

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,20 +1,46 @@
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Button, Container, Grid } from '@mui/material';
 import React from 'react';
 import './Homepage.css'
 import firstImm from '../../images/img.png'
+import { pointsArray } from './pointsArry';
 
 const Homepage = () => {
     return (
     <div>
           <Container sx={{display:'inline-flex',alignItems:'center', flexWrap:{sm:'wrap-reverse',lg:'nowrap',xs:"wrap-reverse"}}}>
-            <Grid lg={6} md={6} sm={10}xs={10}>
+            <Grid lg={2} md={6} sm={4}xs={4}>
                 <Box className='first-screen__title' component='h1'>
                 Make money with MelBet TeamCash!
                 </Box>
+                <Box component='p' sx={{fontWeight:'bolder'}}>
+                PARTNERING UP WITH AN INTERNATIONAL BOOKMAKER IS INCREDIBLY REWARDING. ACCEPT FUNDS, TOP UP ACCOUNTS, MAKE WITHDRAWALS FOR CUSTOMERS OR CREATE YOUR VERY OWN AGENT NETWORK AND EARN COMMISSION!
+                </Box>
+                <Button sx={{
+                marginRight:'2em',
+                backgroundColor: '#FEBD02',
+                borderRadius: '3px',
+                fontWeight: 500,
+                fontSize: '18px',
+                lineHeight: '21px',
+                textAlign: 'center',
+                textTransform: 'uppercase',
+                color: '#000000',
+                textShadow: '0 2px 0 #FFCF44',
+                height:'1em',
+                margin:'0.8em',
+                padding:'1em'          
+              }}>BECOME AN AGENT</Button>
             </Grid>
-            <Grid lg={6} md={6} sm={10}xs={10}>
-                <img style={{width:'100%'}} src={firstImm} alt="asd" />
+            <Grid  lg={10} md={6} sm={10}xs={10}>
+                <img  src={firstImm} alt="asd" />
             </Grid>
+          </Container>
+          <Container>
+              {pointsArray.map((point )=>
+               <Box sx={{display:'inline-flex'}}>
+                <Box>
+                <Box component={'img'} src={point.image}></Box></Box></Box>
+              )}
           </Container>
             
         </div>
---
+++ src/pages/home/pointsArry.js
@@ -0,0 +1,30 @@
+import bookmaker from '../../images/bookmaker.png'
+import contries from '../../images/countries.png'
+import fourhk from '../../images/400k.png'
+import bestTerms from '../../images/bestTerms.png'
+import stablePayOuts from '../../images/stablePayout.png'
+
+
+export const pointsArray = [
+    {
+        title: "RELIABLE BOOKMAKER SINCE 2012",
+        image: bookmaker
+    },
+   
+    {
+        title: "50+ COUNTRIES",
+        image: contries
+    },
+    {
+        title: "400,000+ PLAYERS WORLDWIDE",
+        image: fourhk
+    },
+    {
+        title: "BEST TERMS",
+        image: bestTerms
+    },
+    {
+        title: "STABLE PAYOUTS",
+        image:stablePayOuts
+    }
+];
