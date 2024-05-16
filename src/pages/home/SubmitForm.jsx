69b7b384336bb0bc8b7940306f4c4b8db3dfe5c5
Emtiaz-pias <emtiazpias383@gmail.com>
2024-05-17 00:15:45 +0600

    radio worked

--- src/pages/home/SubmitForm.jsx
+++ src/pages/home/SubmitForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
@@ -11,25 +11,31 @@ import { Button } from '@mui/material';
 const  SubmitForm = ()=> {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
-  console.log(errors);
+  const [isTelegramChecked, setIsTelegramChecked] = useState(false);
+
+  const handleRadioChange = (event) => {
+    setIsTelegramChecked(event.target.value === 'Telegram');
+  }
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
 
-      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps' placeholder="Your name"  required id="outlined-required"label="Name" {...register("Your name", {required: true})} /> <br />
-      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Country" placeholder="Country"  {...register("Country", {required: true})} /> <br />
-      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Email" placeholder="Your email"  {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} /> <br />
-      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Phone" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} /> <br />
+      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps' placeholder="Your name"  required id="outlined-required"label="Name" {...register("Your name", {required: true})} /> <br />{errors && <span className="error-message">Please fill out this field.</span>}
+      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Country" placeholder="Country"  {...register("Country", {required: true})} /> <br />{errors && <span className="error-message">Please fill out this field.</span>}
+      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Email" placeholder="Your email"  {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} /><br />{errors && <span className="error-message">Please fill out this field.</span>}
+      <TextField   sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'  required id="outlined-required"label="Phone" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} /> <br />{errors && <span className="error-message">Please fill out this field.</span>}
       <FormControl>
       <RadioGroup
         row 
-        sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'     
+        sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps'
+        value={isTelegramChecked ? "Telegram" : "Other"}
+        onChange={handleRadioChange}     
       >
         <FormControlLabel value="Telegram" control={<Radio />} label="Telegram" {...register("Telegram", { required: true })} /><br />
         <FormControlLabel value="Other" control={<Radio />} label="Other" {...register("Other", { required: true })} /><br />
       </RadioGroup>
     </FormControl> <br />    
-      <TextField  sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps' required id="outlined-required"label="Nick Name" placeholder="Nickname" {...register("Nickname", {required: true})} /><br />
+      <TextField  sx={{width:{lg:"30vw !important",xs:"80vw !important"},margin:'0em 1em'}} className='inps' required id="outlined-required"label="Nickname" placeholder={isTelegramChecked ? "@" : ""} {...register("Nickname", {required: true})} /><br />{errors && <span className="error-message">Please fill out this field.</span>} <br />
       <Button type='submit' className='becomeAgentBttton' style={{
                 backgroundColor: '#FEBD02',
                 borderRadius: '3px',
