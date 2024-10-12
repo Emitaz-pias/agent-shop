c42e01b94beb91493bcc8f8bc66313264d72c89f
Emtiaz Pias <61836021+Emitaz-pias@users.noreply.github.com>
2024-10-12 23:08:57 +0600

    Update SubmitForm.jsx
    Iur sheet connected

--- src/pages/home/SubmitForm.jsx
+++ src/pages/home/SubmitForm.jsx
@@ -15,7 +15,7 @@ import { AppContext } from '../../AppContext';
   const translations = require(`../../translations/${language.toLowerCase()}.json`);
  
   const createSheetData = (data) => {
-    fetch('https://sheetdb.io/api/v1/n5fjzxbfyd2zo', {
+    fetch('https://sheetdb.io/api/v1/gcswd3wirgcj1', {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
